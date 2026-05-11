b4b7b0b5ea52338c96fd5843bd0aba8abd4a8e16
Daniel <daniel.kolesnitski@focisolutions.com>
2026-05-10 20:59:20 -0700

    Added Favicon, added sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({ routesDirectory: './src/routes' }),
+    TanStackRouterVite({ routesDirectory: "./src/routes" }),
     react(),
     tailwindcss(),
+    sitemap({ hostname: "https://www.thepitbox.ca" }),
   ],
   server: {
     port: 5173,
   },
-})
+});
